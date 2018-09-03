697ab42ab541169b182075fd3da16dcb2c6f8efe
mishantrop <quasi-art@ya.ru>
2018-09-04 02:23:40 +0300

    dynamic

--- src/options.js
+++ src/options.js
@@ -2,20 +2,6 @@
 function saveOptions() {
   console.group('saveOptions');
   saveOptionsManagersTable();
-
-  // var color = document.getElementById('color').value;
-  // var likesColor = document.getElementById('like').checked;
-  // chrome.storage.sync.set({
-  //   favoriteColor: color,
-  //   likesColor: likesColor
-  // }, function() {
-  //   // Update status to let user know options were saved.
-  //   var status = document.getElementById('status');
-  //   status.textContent = 'Options saved.';
-  //   setTimeout(function() {
-  //     status.textContent = '';
-  //   }, 750);
-  // });
   console.groupEnd('saveOptions');
 }
 
@@ -50,42 +36,13 @@ function saveOptionsManagersTable() {
 function renderPathesTable() {
   console.group('renderPathesTable');
   const managerTable = document.getElementById('managers-pathes');
-  const tbody = managerTable ? managerTable.querySelector('tbody') : null;
-  if (managerTable && tbody) {
+  if (managerTable) {
     chrome.storage.sync.get('pathes', function(result) {
       console.log(result);
       if (result && 'pathes' in result && Array.isArray(result.pathes)) {
         console.log(result.pathes);
         result.pathes.forEach((path) => {
-          const newRow = tbody.insertRow(0);
-
-          // First cell
-          // <input type="text" data-name="site_url" value="https://github.com/" />
-          const newCell1 = newRow.insertCell(0);
-          const input1 = document.createElement('input');
-          input1.setAttribute('data-name', 'site_url');
-          input1.type = 'text';
-          input1.value = path.siteUrl;
-          newCell1.appendChild(input1);
-
-          const newCell2 = newRow.insertCell(0);
-          const input2 = document.createElement('input');
-          input2.setAttribute('data-name', 'manager_path');
-          input2.type = 'text';
-          input2.value = path.managerPath;
-          newCell2.appendChild(input2);
-
-          const newCell3 = newRow.insertCell(0);
-          newCell3.innerHTML = path.siteUrl + path.managerPath;
-
-          const newCell4 = newRow.insertCell(0);
-          const buttonRemove = document.createElement('button');
-          buttonRemove.setAttribute('type', 'button');
-          buttonRemove.className = 'button-remove';
-          buttonRemove.addEventListener('click', () => {
-            console.log('removing...');
-          });
-          newCell4.appendChild(buttonRemove);
+          insertPathesTableRow(managerTable, path);
         });
       } else {
         console.warn('Pathes not found in storage');
@@ -97,22 +54,86 @@ function renderPathesTable() {
   console.groupEnd('renderPathesTable');
 }
 
+function insertPathesTableRow(managerTable, path) {
+  const tbody = managerTable ? managerTable.querySelector('tbody') : null;
+  const newRow = tbody.insertRow(getRowsCount(managerTable));
+  newRow.setAttribute('data-record', '');
+
+  // First cell
+  const newCell1 = newRow.insertCell(0);
+  const input1 = document.createElement('input');
+  input1.setAttribute('data-name', 'site_url');
+  input1.type = 'text';
+  input1.value = path.siteUrl;
+  input1.addEventListener('input', () => {
+    refreshFullUris(newRow);
+  })
+  newCell1.appendChild(input1);
+
+  // Second cell
+  const newCell2 = newRow.insertCell(1);
+  const input2 = document.createElement('input');
+  input2.setAttribute('data-name', 'manager_path');
+  input2.type = 'text';
+  input2.value = path.managerPath;
+  input2.addEventListener('input', () => {
+    refreshFullUris(newRow);
+  })
+  newCell2.appendChild(input2);
+
+  // Third cell
+  const newCell3 = newRow.insertCell(2);
+  const link = document.createElement('a');
+  link.setAttribute('href', generateManagerUri(path.siteUrl, path.managerPath));
+  link.setAttribute('target', '_blank');
+  link.innerHTML = generateManagerUri(path.siteUrl, path.managerPath);
+  newCell3.appendChild(link);
+
+  // Fourth cell
+  const newCell4 = newRow.insertCell(3);
+  const buttonRemove = document.createElement('button');
+  buttonRemove.setAttribute('type', 'button');
+  buttonRemove.className = 'button-remove';
+  buttonRemove.innerHTML = '&times;';
+  buttonRemove.addEventListener('click', () => {
+    console.log('removing...');
+  });
+  newCell4.appendChild(buttonRemove);
+}
+
+function generateManagerUri(siteUrl, managerPath) {
+  return siteUrl + managerPath;
+}
+
+function getRowsCount(managerTable) {
+  if (managerTable) {
+    const rows = managerTable.querySelectorAll('tr[data-record]');
+    console.log(rows.length);
+    return rows ? rows.length : 0;
+  }
+  return 0;
+}
+
+function refreshFullUris(row) {
+  if (row) {
+    const link = row.querySelector('a');
+    const siteUrl = row.querySelector('input[data-name="site_url"]').value;
+    const managerPath = row.querySelector('input[data-name="manager_path"]').value;
+    if (link) {
+      link.setAttribute('href', generateManagerUri(siteUrl, managerPath));
+      link.innerHTML = generateManagerUri(siteUrl, managerPath);
+    }
+  }
+}
+
 function render() {
   renderPathesTable();
 }
 
-// function restore_options() {
-//   chrome.storage.sync.get({
-//     favoriteColor: 'red',
-//     likesColor: true
-//   }, function(items) {
-//     document.getElementById('color').value = items.favoriteColor;
-//     document.getElementById('like').checked = items.likesColor;
-//   });
-// }
-
-// document.addEventListener('DOMContentLoaded', restore_options);
-
 render();
 
 document.getElementById('save').addEventListener('click', saveOptions);
+document.getElementById('add-record').addEventListener('click', () => {
+  const managerTable = document.getElementById('managers-pathes');
+  insertPathesTableRow(managerTable, { siteUrl: '', managerPath: '' });
+});
