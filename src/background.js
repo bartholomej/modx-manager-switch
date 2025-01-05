618b6463eb6f8e0a6e20204f12ad02182e69391e
BART! <bart@bartweb.cz>
2025-01-05 12:04:06 +0100

    feat(manifest): update to manifest v3

--- src/background.js
+++ src/background.js
@@ -1,55 +1,50 @@
-// Actions onClicked
-chrome.browserAction.onClicked.addListener(function(tab) {
-  const tab_url = tab.url;
+chrome.action.onClicked.addListener((tab) => {
+  const tabUrl = tab.url;
   const tabIndex = tab.index;
   let newUrl;
 
-  const path = tab_url.split('/');
+  const path = tabUrl.split('/');
   const protocol = path[0];
   const host = path[2];
   const uri = path[3];
-  const base = protocol + '//' + host;
+  const base = `${protocol}//${host}`;
 
-  chrome.tabs.sendMessage(tab.id, {
-      command: "getManager"
-    },
-    function(sysObject) {
-      let createTab = false;
-      //  if Queeg is detected, go to manager like a boss!
-      if (sysObject) {
-        newUrl = sysObject.host + sysObject.manager;
-        newUrl += '?a=resource/update&id=' + sysObject.id;
-        createTab = true;
-      } else {
-        // w/o Queeg: Just try to leave manager
-        if (uri == 'manager') {
-          newUrl = base;
-          chrome.browserAction.setTitle({
-            title: "Preview MODX website"
-          });
-          createTab = true;
-        } else {
-          // w/o Queeg: Just try to open manager
-          const managerPath = findManagerPath(host, base, (newUrl) => {
-            chrome.tabs.create({
-                url: newUrl,
-                index: tabIndex + 1
-            });
-          });
-          // newUrl = base + '/manager';
-        }
-      }
-
-      if (createTab) {
-        chrome.tabs.create({
-            url: newUrl,
-            index: tabIndex + 1
+  chrome.tabs.sendMessage(tab.id, { 
+      command: 'getManager' 
+    }, 
+    (sysObject) => {
+    let createTab = false;
+    //  if Queeg is detected, go to manager like a boss!
+    if (sysObject) {
+      newUrl = `${sysObject.host}${sysObject.manager}?a=resource/update&id=${sysObject.id}`;
+      createTab = true;
+      // w/o Queeg: Just try to leave manager
+    } else if (uri === 'manager') {
+      newUrl = base;
+      chrome.action.setTitle({ 
+        title: 'Preview MODX website' 
+      });
+      createTab = true;
+    } else {
+      // w/o Queeg: Just try to open manager
+      findManagerPath(host, base, (newUrl) => {
+        chrome.tabs.create({ 
+          url: newUrl, 
+          index: tabIndex + 1 
         });
-      }
-    });
+      });
+    }
+
+    if (createTab) {
+      chrome.tabs.create({ 
+        url: newUrl, 
+        index: tabIndex + 1 
+      });
+    }
+  });
 });
 
-cleanProtocolEtc = (host) => {
+const cleanProtocolEtc = (host) => {
   return host
     .replace('https://www.', '')
     .replace('http://www.', '')
@@ -57,18 +52,18 @@ cleanProtocolEtc = (host) => {
     .replace('http://', '');
 }
 
-findManagerPath = (hostInput, base, callbackSuccess) => {
+const findManagerPath = (hostInput, base, callbackSuccess) => {
   let managerPath = 'manager';
-  let newUrl = base + '/' + managerPath;
-  let host = cleanProtocolEtc(hostInput);
+  let newUrl = `${base}/${managerPath}`;
+  const host = cleanProtocolEtc(hostInput);
 
-  chrome.storage.sync.get('paths', function(result) {
-    if (result && 'paths' in result && Array.isArray(result.paths)) {
+  chrome.storage.sync.get('paths', (result) => {
+    if (result && result.paths && Array.isArray(result.paths)) {
       result.paths.forEach((path) => {
         path.siteUrl = cleanProtocolEtc(path.siteUrl);
-        if (host.indexOf(path.siteUrl) >= 0) {
+        if (host.includes(path.siteUrl)) {
           managerPath = path.managerPath;
-          newUrl = base + '/' + managerPath;
+          newUrl = `${base}/${managerPath}`;
         }
       });
     } else {
@@ -76,100 +71,91 @@ findManagerPath = (hostInput, base, callbackSuccess) => {
     }
     callbackSuccess(newUrl);
   });
-}
+};
 
-chrome.runtime.onMessage.addListener(
-  function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  let badgeColor;
 
-    // Process system object
-    if (message.system) {
-      delete message.system;
+  // Process system object
+  if (message.system) {
+    delete message.system;
 
-      if (message.published === 0) {
-        badge_color = error_color
-      };
-      if (message.published === 1) {
-        badge_color = success_color
-      };
+    badgeColor = message.published === 0 ? errorColor : successColor;
 
-      animateFlip(sender.tab.id);
+    animateFlip(sender.tab.id);
 
-      chrome.browserAction.setBadgeText({
-        text: message.id.toString(),
-        tabId: sender.tab.id
-      });
+    chrome.action.setBadgeText({ 
+      text: message.id.toString(), 
+      tabId: sender.tab.id 
+    });
+    chrome.action.setBadgeBackgroundColor({ 
+      color: badgeColor, 
+      tabId: sender.tab.id 
+    });
+  } else {
+    // Process content object
+    delete message.system;
+    const tooltip = Object.entries(message).map(([key, value]) => `${key}: ${value}`).join('\n');
 
-      chrome.browserAction.setBadgeBackgroundColor({
-        color: badge_color,
-        tabId: sender.tab.id
-      })
-    } else {
-      // Process content object
-      delete message.system;
-
-      var tooltip = [];
-      for (var key in message) {
-        tooltip.push(key + ": " + message[key]);
-      }
-
-      chrome.browserAction.setTitle({
-        title: tooltip.join('\n'),
-        tabId: sender.tab.id
-      });
-    }
+    chrome.action.setTitle({ 
+      title: tooltip, 
+      tabId: sender.tab.id 
+    });
   }
-);
+});
 
 // Change badge and icon onMessage
-var badge_color = [0, 0, 0, 255];
-var success_color = [95, 181, 77, 255];
-var error_color = [229, 62, 48, 255];
+const badgeColor = [0, 0, 0, 255];
+const successColor = [95, 181, 77, 255];
+const errorColor = [229, 62, 48, 255];
+const animationFrames = 36;
+const animationSpeed = 20;
+let rotation = 0;
+let tab;
 
-var animationFrames = 36;
-var animationSpeed = 20; // ms
-var canvas = document.getElementById('canvas');
-var icon = document.getElementById('icon');
-var canvasContext = canvas.getContext('2d');
-var rotation = 0;
-var tab;
-
-function resetActiveIcon(tabId) {
-  chrome.browserAction.setIcon({
-    path: {
-      "19": "images/icon19.png",
-      "38": "images/icon38.png"
-    },
-    tabId: tabId
+const resetActiveIcon = (tabId) => {
+  chrome.action.setIcon({ 
+    path: { 
+      '19': 'images/icon19.png', 
+      '38': 'images/icon38.png' 
+    }, 
+    tabId 
   });
-}
+};
 
-function animateFlip(tabId) {
+const animateFlip = (tabId) => {
   tab = tabId || tab;
   rotation += 1 / animationFrames;
   drawIconAtRotation(tab);
 
   if (rotation <= 1) {
-    setTimeout(animateFlip, animationSpeed);
+    setTimeout(() => animateFlip(tab), animationSpeed);
   } else {
     rotation = 0;
     resetActiveIcon(tab);
   }
-}
+};
 
-function ease(x) {
-  return (1 - Math.sin(Math.PI / 2 + x * Math.PI)) / 2;
-}
+const ease = (x) => (1 - Math.sin(Math.PI / 2 + x * Math.PI)) / 2;
 
-function drawIconAtRotation(tabId) {
-  canvasContext.save();
-  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-  canvasContext.translate(Math.ceil(canvas.width / 2), Math.ceil(canvas.height / 2));
-  canvasContext.rotate(2 * Math.PI * ease(rotation));
-  canvasContext.drawImage(icon, -Math.ceil(canvas.width / 2), -Math.ceil(canvas.height / 2));
-  canvasContext.restore();
+const drawIconAtRotation = async (tabId) => {
+  const canvas = new OffscreenCanvas(38, 38);
+  const ctx = canvas.getContext('2d');
 
-  chrome.browserAction.setIcon({
-    imageData: canvasContext.getImageData(0, 0, canvas.width, canvas.height),
-    tabId: tabId
+  // Fetch the image data
+  const response = await fetch(chrome.runtime.getURL('images/icon38.png'));
+  const blob = await response.blob();
+  const bitmap = await createImageBitmap(blob);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.save();
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.rotate(2 * Math.PI * ease(rotation));
+  ctx.drawImage(bitmap, -canvas.width / 2, -canvas.height / 2);
+  ctx.restore();
+
+  chrome.action.setIcon({
+    imageData: ctx.getImageData(0, 0, canvas.width, canvas.height),
+    tabId: tabId,
   });
-}
+};
